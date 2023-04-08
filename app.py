79a418c07e67d2e8ea016e89e4bf6b5c0fa95547
abandomingo <abdomingo@csu.fullerton.edu>
2023-04-08 01:42:22 -0700

    Added Comments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+# import required modules
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
 from flask_cors import CORS
 import jwt
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 import pymysql
+
+# load environment variables from .env file
 load_dotenv()
 
 import os
@@ -19,10 +22,15 @@ app = Flask(__name__)
 # CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+
+# set the secret key for generating and decoding JWT tokens
 app.secret_key = os.getenv('JWT_SECRET_KEY')
+
+# set the upload folder and maximum content length
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1000 * 1000 #8 megabytes max
 
+# connect to the MySQL database CHANGE THIS TO YOUR DB SETTINGS
 conn = pymysql.connect(
         host='127.0.0.1',
         user='root', 
@@ -32,6 +40,7 @@ conn = pymysql.connect(
 
 cur = conn.cursor()
 
+# function to generate JWT tokens
 def generate_jwt_token(content):
     encoded_content = jwt.encode(content, app.secret_key, algorithm="HS256")
     token = str(encoded_content)
@@ -45,6 +54,7 @@ def decode_user(token: str):
 @app.route('/login', methods =['GET', 'POST'])
 def login():
   if request.method == 'GET':
+    # if a GET request is received, fetch all accounts from the database and return them as a list of JSON strings
     cur.execute('SELECT * FROM accounts')
     accounts = cur.fetchall()
     accountsList = []
@@ -52,10 +62,12 @@ def login():
       accountsList.append(json.dumps(row))
     return accountsList
   if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    # if a POST request is received and the username and password fields are present, check if the user exists and if the password is correct
     username = request.form['username']
     password = request.form['password']
     cur.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password))
     account = cur.fetchone()
+    # if the account is not found, return a 401 Unauthorized error with a JSON message
     if account == None:
       return make_response(
     jsonify({
@@ -66,8 +78,10 @@ def login():
     )
     this_id = account[0]
     jwt_token = generate_jwt_token({"id": this_id})
+  # if the account is found, generate a JWT token for the user and return it as a JSON message
   if account:
     return jsonify({"jwt_token": jwt_token})
+  # if something went wrong, return a 401 Unauthorized error with a JSON message
   return make_response(
   jsonify({
     "message": "User does not exists", 
@@ -79,10 +93,12 @@ def login():
 @app.route('/admin', methods =['POST'])
 def admin():
     if request.method == 'POST' and 'jwt_token' in request.form:
+         # if a POST request is received and jwt_token field is present, check take the jwt_token
         jwt_token = request.form['jwt_token']
         try:
             obj = decode_user(jwt_token)
         except jwt.exceptions.DecodeError:
+            # if decoded content is not available or jwt_token is wrong, return a 401 Unauthorized error with a JSON message
             return make_response(
                 jsonify({
                     "message": "Invalid token",
@@ -93,7 +109,9 @@ def admin():
         this_id = obj['id']
         cur.execute('SELECT * FROM accounts WHERE id = %s', (this_id))
         account = cur.fetchone()
+        #once the id taken from the decoded content, find in database and return user account info
         return jsonify({'user': account})
+    # if there is no POST Request or no jwt_token in request
     return make_response(
         jsonify({
             "message": "User is not recognized as admin", 
@@ -129,6 +147,7 @@ def upload_file():
 					"data": None
 				}), 400
             )
+        #if the extension is not in the selected approved file types return error
         if not allowed_file(file.filename):
           return make_response(
             jsonify({
@@ -137,17 +156,20 @@ def upload_file():
               "data": file.filename
             }), 406
             )
+        #if it is present and it is valid, upload filename to filepath set before
         if file and allowed_file(file.filename):
           filename = secure_filename(file.filename)
           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
           return 'Success'
+    #if the request is not a post request, give error
     return make_response(
 		jsonify({
-			"message": "Unauthorized user cannot upload file", 
-			"error": "Unauthorized", 
+			"message": "Must be a POST request", 
+			"error": "Method Not Allowed", 
 			"data": None
-		}), 401
+		}), 405
     )
 
+#start app on localhost port 5000
 if __name__ == "__main__":
 	app.run(host ="localhost", port = int("5000"))
